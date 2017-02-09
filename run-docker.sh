046ac87fa463d46c6e35122aee9dfd953af4edaa
Piotr Joński <yoyo@wp.eu>
2017-02-09 21:55:01 +0100

    change ownership

--- run-docker.sh
+++ run-docker.sh
@@ -2,27 +2,28 @@
 
 if [ -z "$1" ]
 then
-    echo "Pass unix username as first argument. This user will receive rights for newly created data folders."
+    echo "Pass unix group name as first argument. This group will receive rights for data folders."
     exit -1;
 fi
 
 JETBRAINS_PRODUCTS=(teamcity hub youtrack upsource)
 JETBRAINS_USER_NAME=jetbrains
 JETBRAINS_USER_ID=2000
-CURRENT_USER_GID=$1
-echo ${CURRENT_USER_GID}
+GROUP_NAME=$1
+echo ${GROUP_NAME}
 
 groupadd --gid ${JETBRAINS_USER_ID} ${JETBRAINS_USER_NAME}
 useradd --system --uid ${JETBRAINS_USER_ID} --gid ${JETBRAINS_USER_NAME} ${JETBRAINS_USER_NAME}
 
 for item in ${JETBRAINS_PRODUCTS[*]}
 do
-    mkdir -vp ${item}/backups
+    mkdir -v  ${item}/backups
     mkdir -v  ${item}/data
     mkdir -v  ${item}/log
     mkdir -v  ${item}/tmp
     mkdir -v  ${item}/conf
-    chown --changes --recursive ${JETBRAINS_USER_NAME}:${CURRENT_USER_GID} ${item}
+    chown --changes --recursive ${JETBRAINS_USER_NAME}:${GROUP_NAME} ${item}
+    chmod --recursive 770 ${item}
 done
 
 docker-compose up -d
